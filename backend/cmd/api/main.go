ca5866d98cf6ced7435f4ff2eff0f88b5b553bb4
nathantotten <nathan.james.totten@gmail.com>
2026-04-28 22:04:51 +0100

    Move to explicit serve mux

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -9,9 +9,10 @@ import (
 func main() {
 	log.Println("Starting server on :8080")
 
-	http.HandleFunc("/health", healthCheck)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/health", healthCheck)
 
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", mux)
 }
 
 func healthCheck(w http.ResponseWriter, r *http.Request) {
